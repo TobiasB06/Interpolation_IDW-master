e0a9c94d472df8c9ed7860318ee9103b78e0561e
Tobias <tobiasbonanno@gmail.com>
2023-10-13 18:01:58 -0300

    Cambio en perfiles angulares

--- Interpolation.py
+++ Interpolation.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk,filedialog
 import Read_excel
 
 
@@ -7,7 +8,17 @@ class Interpolacion_Interfaz():
         self.root = root
         self.root.title("Interpolacion")
         self.root.geometry("400x400")
-        
+        self.widgets()
+    def widgets(self):
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure((0,2), weight=2)
+        self.root.rowconfigure(1, weight=1)
+        self.frame_excel = ttk.Labelframe(self.root,text="Excel")
+        self.frame_archivo_interp=ttk.Labelframe(self.root,text="Archivo a interpolar")
+        self.frame_resultados=ttk.Labelframe(self.root,text="Resultado")
+        self.frame_excel.grid(row=0, column=0, sticky="nsew", padx=5, pady=2)
+        self.frame_archivo_interp.grid(row=1, column=0, sticky="nsew", padx=5, pady=2)
+        self.frame_resultados.grid(row=2, column=0, sticky="nsew", padx=5, pady=2)
 if __name__ == '__main__':
     root1 = tk.Tk()
     Interpolacion_Interfaz(root1)
